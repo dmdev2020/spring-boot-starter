e6563af5f410e7e2631aec6c27413200e0a82b04
dmdev2020 <dmdev2020@gmail.com>
2021-12-25 15:42:08 +0100

    added lesson-3.1

--- src/main/java/com/dmdev/springboot/lesson/config/ApplicationConfiguration.java
+++ src/main/java/com/dmdev/springboot/lesson/config/ApplicationConfiguration.java
@@ -1,20 +1,13 @@
 package com.dmdev.springboot.lesson.config;
 
-import com.dmdev.springboot.lesson.conditional.FirstConditional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Conditional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import javax.annotation.PostConstruct;
 
-@Conditional(FirstConditional.class)
-@Profile("dev")
+@Slf4j
 @Configuration
 public class ApplicationConfiguration {
-
-    private static final Logger log = LoggerFactory.getLogger(ApplicationConfiguration.class);
 
     @PostConstruct
     public void init() {
