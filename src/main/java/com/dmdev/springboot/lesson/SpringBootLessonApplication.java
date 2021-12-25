a9d226b6d5b63b6f60985de6e0fcfb1ef2bdb183
dmdev2020 <dmdev2020@gmail.com>
2021-12-25 15:37:38 +0100

    added lesson-1

---
+++ src/main/java/com/dmdev/springboot/lesson/SpringBootLessonApplication.java
@@ -0,0 +1,12 @@
+package com.dmdev.springboot.lesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLessonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLessonApplication.class, args);
+    }
+}
